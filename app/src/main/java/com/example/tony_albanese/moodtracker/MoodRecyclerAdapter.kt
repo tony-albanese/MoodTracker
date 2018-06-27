e8d1961d1a03a3201ec1a70192c4a24b7b287a77
tony-albanese <tony.albanese2000@gmail.com>
2018-06-27 21:30:30 +0200

    Added dummy data.

--- app/src/main/java/com/example/tony_albanese/moodtracker/MoodRecyclerAdapter.kt
+++ app/src/main/java/com/example/tony_albanese/moodtracker/MoodRecyclerAdapter.kt
@@ -11,7 +11,14 @@ import android.widget.TextView
 /* This class defines the RecyclerView Adapter object that will handle the moods the user can select from. This is NOT the adapter for the moods the user enters.*/
 class MoodRecyclerAdapter : RecyclerView.Adapter<MoodRecyclerAdapter.ViewHolder>(){
 
-        inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    //Will include some dummy data here that will NOT be included in the final project.
+    private val moods = arrayOf("Feeling Great!", "Feeling OK" )
+    private val images = intArrayOf(R.mipmap.smiley_super_happy, R.mipmap.smiley_normal)
+
+        inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+            //Empty
+        }
 
 
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
