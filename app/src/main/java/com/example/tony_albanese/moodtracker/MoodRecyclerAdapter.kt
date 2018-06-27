1e47a44850796abce55e8cd27c235962bde74bcc
tony-albanese <tony.albanese2000@gmail.com>
2018-06-27 21:22:46 +0200

    Added comment to MoodRecyclerAdapter class.

---
+++ app/src/main/java/com/example/tony_albanese/moodtracker/MoodRecyclerAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.tony_albanese.moodtracker
+
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.RecyclerView.Adapter
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+
+/* This class defines the RecyclerView Adapter object that will handle the moods the user can select from. This is NOT the adapter for the moods the user enters.*/
+class MoodRecyclerAdapter : RecyclerView.Adapter<MoodRecyclerAdapter.ViewHolder>(){
+
+        inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
