2ebb38877ee3113d062b5b648c341fbf4133a782
tony-albanese <tony.albanese2000@gmail.com>
2018-07-01 21:32:06 +0200

    Added id to recycler_view and implemented adapter and layout manager.

--- app/src/main/java/com/example/tony_albanese/moodtracker/MainActivity.kt
+++ app/src/main/java/com/example/tony_albanese/moodtracker/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.tony_albanese.moodtracker
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<MoodRecyclerAdapter.ViewHolder>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        layoutManager = LinearLayoutManager(this)
+        recycler_view.layoutManager = layoutManager
+
+        adapter = MoodRecyclerAdapter()
+        recycler_view.adapter = adapter
     }
 }
