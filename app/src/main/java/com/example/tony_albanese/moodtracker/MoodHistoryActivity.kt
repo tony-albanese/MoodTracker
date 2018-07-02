cd2b1ed6f5dbd7d08072ef60f595e4f5eb6c0518
tony-albanese <tony.albanese2000@gmail.com>
2018-07-02 21:46:18 +0200

    Implemented adapter for recycler view layout for MoodHistory Activity

--- app/src/main/java/com/example/tony_albanese/moodtracker/MoodHistoryActivity.kt
+++ app/src/main/java/com/example/tony_albanese/moodtracker/MoodHistoryActivity.kt
@@ -2,11 +2,24 @@ package com.example.tony_albanese.moodtracker
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_mood_history.*
 
 class MoodHistoryActivity : AppCompatActivity() {
+
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<MoodHistoryRecyclerAdapter.ViewHolder>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mood_history)
+
+        layoutManager = LinearLayoutManager(this)
+        mood_history_recycler_view.layoutManager = layoutManager
+
+        adapter = MoodHistoryRecyclerAdapter()
+        mood_history_recycler_view.adapter = adapter
+
     }
 }
