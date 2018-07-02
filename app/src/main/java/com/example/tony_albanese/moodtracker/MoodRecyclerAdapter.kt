d2a4c8e04ff7859b4a4a693c966bf516e9bd0abc
tony-albanese <tony.albanese2000@gmail.com>
2018-07-02 13:30:38 +0200

    Added onClickListener to MainActivity

--- app/src/main/java/com/example/tony_albanese/moodtracker/MoodRecyclerAdapter.kt
+++ app/src/main/java/com/example/tony_albanese/moodtracker/MoodRecyclerAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.tony_albanese.moodtracker
 
+import android.support.design.widget.Snackbar
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -9,7 +10,6 @@ import android.widget.TextView
 
 /* This class defines the RecyclerView Adapter object that will handle the moods the user can select from. This is NOT the adapter for the moods the user enters.*/
 class MoodRecyclerAdapter : RecyclerView.Adapter<MoodRecyclerAdapter.ViewHolder>(){
-
 
     //Will include some dummy data here that will NOT be included in the final project.
     private val moods = arrayOf("Feeling Great!", "Feeling OK" )
@@ -23,6 +23,13 @@ class MoodRecyclerAdapter : RecyclerView.Adapter<MoodRecyclerAdapter.ViewHolder>
             init {
                 imageItem = itemView.findViewById(R.id.card_mood_image)
                 itemText = itemView.findViewById(R.id.card_mood_text)
+
+                itemView.setOnClickListener{ v: View ->
+                    var position: Int = adapterPosition
+                    Snackbar.make(v, moods[position], Snackbar.LENGTH_LONG).setAction("Action", null).show()
+
+
+                }
             }
         }
 
@@ -33,7 +40,7 @@ class MoodRecyclerAdapter : RecyclerView.Adapter<MoodRecyclerAdapter.ViewHolder>
     }
 
     override fun getItemCount(): Int {
-        return 2
+        return moods.size
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, i: Int) {
