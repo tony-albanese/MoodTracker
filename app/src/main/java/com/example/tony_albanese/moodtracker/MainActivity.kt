2cd805099da9ee09da511838edc104ce05b8966f
tony-albanese <tony.albanese2000@gmail.com>
2018-07-02 15:41:34 +0200

    Added intent navigation to fab so the mood history Activity is launched. Added MoodHistoryActivity.

--- app/src/main/java/com/example/tony_albanese/moodtracker/MainActivity.kt
+++ app/src/main/java/com/example/tony_albanese/moodtracker/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.tony_albanese.moodtracker
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,5 +23,12 @@ class MainActivity : AppCompatActivity() {
 
         adapter = MoodRecyclerAdapter()
         recycler_view.adapter = adapter
+
+        //Set the click listener for the fab to navigate to the MoodHistoryActivity.
+
+        root_frame_layout.fab_mood_history.setOnClickListener { v: View ->
+            val intent: Intent = Intent(this, MoodHistoryActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/tony_albanese/moodtracker/MoodHistoryActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tony_albanese.moodtracker
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MoodHistoryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mood_history)
+    }
+}
