fd90b4ebe5bf0a7847a5d0b4c2202d1b6c52f05f
tony-albanese <tony.albanese2000@gmail.com>
2018-07-02 21:38:22 +0200

    Implemented override methods.

--- app/src/main/java/com/example/tony_albanese/moodtracker/MoodHistoryRecyclerAdapter.kt
+++ app/src/main/java/com/example/tony_albanese/moodtracker/MoodHistoryRecyclerAdapter.kt
@@ -1,24 +1,32 @@
 package com.example.tony_albanese.moodtracker
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 class MoodHistoryRecyclerAdapter : RecyclerView.Adapter<MoodHistoryRecyclerAdapter.ViewHolder>() {
-
+    private var dates = arrayOf("Date 1", "Date 2", "Date 3")
     inner class ViewHolder(moodHistoryItemView: View): RecyclerView.ViewHolder(moodHistoryItemView){
 
+
+        var historyDateTextView : TextView
+        init {
+            historyDateTextView = moodHistoryItemView.findViewById(R.id.mood_history_date)
+        }
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return dates.size
     }
 
-    override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onBindViewHolder(viewHolder: ViewHolder, i: Int) {
+        viewHolder.historyDateTextView.text = dates[i]
     }
 
-    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
+        val v = LayoutInflater.from(viewGroup.context).inflate(R.layout.mood_history_card_layout, viewGroup, false)
+        return ViewHolder(v)
     }
 }
