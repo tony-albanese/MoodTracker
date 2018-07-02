491650552ea861df9e56e87d9258329e8a93da11
tony-albanese <tony.albanese2000@gmail.com>
2018-07-02 21:15:48 +0200

    Added ViewHolder inner class and outlined override functions. (Not yet implemented.)

---
+++ app/src/main/java/com/example/tony_albanese/moodtracker/MoodHistoryRecyclerAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.tony_albanese.moodtracker
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+class MoodHistoryRecyclerAdapter : RecyclerView.Adapter<MoodHistoryRecyclerAdapter.ViewHolder>() {
+
+    inner class ViewHolder(moodHistoryItemView: View): RecyclerView.ViewHolder(moodHistoryItemView){
+
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
