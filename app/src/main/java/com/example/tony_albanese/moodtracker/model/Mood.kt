e0a229b864e7163406c376c922d71775538659c4
tony-albanese <tony.albanese2000@gmail.com>
2018-07-03 11:52:21 +0200

    Added generateMoodSelection() function to MainActivity.kt.

--- app/src/main/java/com/example/tony_albanese/moodtracker/model/Mood.kt
+++ app/src/main/java/com/example/tony_albanese/moodtracker/model/Mood.kt
@@ -1,13 +1,16 @@
 package com.example.tony_albanese.moodtracker.model
 
-public class Mood(description: String = "No description specified", imageID: Int = 0)   {
+public class Mood(description: String = "No description specified", imageID: Int, backgroundColor: Int)   {
     val mDescription: String
     val mImageId: Int
+    val mBackgoundColor: Int
 
 
     init {
         mDescription = description
         mImageId = imageID
+        mBackgoundColor = backgroundColor
     }
 
 }
+
