74d10c4012285e1629d32c16437dc08d7544fa39
tony-albanese <tony.albanese2000@gmail.com>
2018-07-05 21:31:30 +0200

    Got rid of a TODO.

--- app/src/main/java/com/example/tony_albanese/moodtracker/model/DailyMood.kt
+++ app/src/main/java/com/example/tony_albanese/moodtracker/model/DailyMood.kt
@@ -3,7 +3,7 @@ package com.example.tony_albanese.moodtracker.model
 
 class DailyMood(description: String = "No description specified", imageID: Int, backgroundColor: Int, comment: String = "", date: String) : Mood(description,imageID,backgroundColor) {
 
-    val mComment: String //TODO: Find out how to safely allow this string to be null.
+    val mComment: String
     val mDate: String
 
     init {
