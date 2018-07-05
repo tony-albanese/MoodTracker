84c2ae687b884707c02885a72a0d1de382595a4b
tony-albanese <tony.albanese2000@gmail.com>
2018-07-05 13:33:49 +0200

    Implemented DailyMood class definition.

---
+++ app/src/main/java/com/example/tony_albanese/moodtracker/model/DailyMood.kt
@@ -0,0 +1,11 @@
+package com.example.tony_albanese.moodtracker.model
+
+class DailyMood(description: String = "No description specified", imageID: Int, backgroundColor: Int, comment: String, date: String): Mood(description,imageID,backgroundColor) {
+    val mComment: String?
+    val mDate: String
+
+    init {
+        mComment = comment
+        mDate = date
+    }
+}
