0170a63f661ffa4d96a45df406c86d0ba16b73cd
tony-albanese <tony.albanese2000@gmail.com>
2018-07-06 06:59:26 +0200

    Added a testlist and generator function to test serialization.

--- app/src/main/java/com/example/tony_albanese/moodtracker/controller/MainActivity.kt
+++ app/src/main/java/com/example/tony_albanese/moodtracker/controller/MainActivity.kt
@@ -15,50 +15,59 @@ import com.example.tony_albanese.moodtracker.model.DailyMood
 import com.example.tony_albanese.moodtracker.model.Mood
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
-import java.io.Serializable
 import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
-    private var moodList =  ArrayList<Mood>(); //This is the object that will contain a collection of Mood objects for display.
+    private var moodList = ArrayList<Mood>(); //This is the object that will contain a collection of Mood objects for display.
     private var layoutManager: RecyclerView.LayoutManager? = null //Create a reference to the layout manager that will organize the views in the RecyclerAdapter.
     private var adapter: RecyclerView.Adapter<MoodRecyclerAdapter.ViewHolder>? = null //Create a reference to our custom adapter.
     lateinit var preferences: SharedPreferences
-    var currentMood: Mood = Mood("I feel happy.", R.mipmap.smiley_happy, R.color.color_happy)
-    val PREFERENCES_KEY_COMMENT: String = "PREFERENCES_KEY_COMMENT"
+    lateinit var currentDailyMood: DailyMood
+    lateinit var todaysDate: Date
+    val PREFERENCES_KEY_DAILY_MOOD: String = "PREFERENCES_KEY_DAILY_MOOD"
     val KEY_DAILY_MOOD_LIST: String = "DAILY_MOOD_LIST"
-    var dailyComment: String = ""
-    val dailyMoodList = ArrayList<DailyMood>()
+    var dailyComment: String = "No comment"
+    var dailyMoodList = ArrayList<DailyMood>()
 
+    var testlist = ArrayList<DailyMood>() //Testlist to see if problem is with serialization.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        todaysDate = Date()
+        currentDailyMood = DailyMood(getString(R.string.mood_happy), R.mipmap.smiley_happy, R.color.color_happy, dailyComment, convertDate(todaysDate))
+        dailyMoodList.add(currentDailyMood)
+
         preferences = getPreferences(Context.MODE_PRIVATE)
-        //TODO: Read the current mood and comment from the SharedPreferences and set a default value if none is there.
+
         generateMoodSelectionList() //Function that populates the ArrayList with Mood objects.
 
         layoutManager = LinearLayoutManager(this) //Our layoutManager holds an instance of a LinearLayoutManager
         recycler_view.layoutManager = layoutManager //Attach the layout manager to the recycler_view.
-
-        adapter = MoodRecyclerAdapter(moodList, {mood: Mood -> moodItemClicked(mood)}) //Initialize our adapter variable with a MoodReyclerAdapter object. We pass in our data as a paramater.
+        adapter = MoodRecyclerAdapter(moodList, { mood: Mood -> moodItemClicked(mood) }) //Initialize our adapter variable with a MoodReyclerAdapter object. We pass in our data as a paramater.
         recycler_view.adapter = adapter //Set the adapter property of the recycler_view to the adapter we just created.
+
+        populateTestList()
 
         //Set the click listener for the fab to navigate to the MoodHistoryActivity.
         root_frame_layout.fab_mood_history.setOnClickListener { v: View ->
             val intent: Intent = Intent(this, MoodHistoryActivity::class.java)
-            //TODO: Make this serializable.
-            intent.putExtra(KEY_DAILY_MOOD_LIST, dailyMoodList as Serializable)
+            //intent.putExtra(KEY_DAILY_MOOD_LIST, testlist as Serializable)
             startActivity(intent)
         }
+
+
         //Set the click listener for the comment fab that will trigger the generation of the dialog.
-        root_frame_layout.fab_add_comment.setOnClickListener {v: View ->
+        root_frame_layout.fab_add_comment.setOnClickListener { v: View ->
             createCommentDialogue()
         }
     }
 
     //This is the function that generates the moods the user can select.
-    fun generateMoodSelectionList(){
-        moodList.add(Mood(getString(R.string.mood_happy),R.mipmap.smiley_happy, R.color.color_happy))
+    fun generateMoodSelectionList() {
+        moodList.add(Mood(getString(R.string.mood_happy), R.mipmap.smiley_happy, R.color.color_happy))
         moodList.add(Mood(getString(R.string.mood_super_happy), R.mipmap.smiley_super_happy, R.color.color_super_happy))
         moodList.add(Mood(getString(R.string.mood_normal), R.mipmap.smiley_normal, R.color.color_normal))
         moodList.add(Mood(getString(R.string.mood_disappointed), R.mipmap.smiley_disappointed, R.color.color_disappointed))
@@ -66,30 +75,36 @@ class MainActivity : AppCompatActivity() {
     }
 
     //This function creates the dialog.
-    fun createCommentDialogue(){
+    fun createCommentDialogue() {
         val commentText: EditText = EditText(this)
         //TODO: Format the EditText object
         val dialog = AlertDialog.Builder(this)
         dialog.setView(commentText)
         dialog.setTitle("How are you feeling?")
                 .setMessage("Enter a comment.")
-                .setNegativeButton("Cancel",null)
-                .setPositiveButton("OK", {dialog, button -> dailyComment = commentText.text.toString()})
+                .setNegativeButton("Cancel", null)
+                .setPositiveButton("OK", { dialog, button -> dailyComment = commentText.text.toString() })
                 .create()
         dialog.show()
-        if(dailyComment.isNotEmpty() || dailyComment.isBlank()) {
-            preferences.edit().putString(PREFERENCES_KEY_COMMENT, dailyComment)
-        }
+
+        //TODO: Fix this logic.
     }
+
 
     //This is the function we want called when the user clicks on a mood in the list.
-    private fun moodItemClicked(mood: Mood){
-        currentMood = mood
-        val dateObject = Date()
-        val date: String = convertDate(dateObject)
-        val message: String = "The current mood has been set to: ${currentMood.mDescription}"
+    private fun moodItemClicked(mood: Mood) {
+        var currentMood: Mood = mood
+        var message = "The current mood has been set to: ${currentMood.mDescription}"
+        //TODO: Fix Logic here.
+
 
         createToast(applicationContext, message)
-        //TODO: This is where the selected mood should be serialized and saved in SharedPreferences.
     }
+
+    private fun populateTestList(){
+        testlist.add(DailyMood("Mood 1", R.mipmap.smiley_happy, R.color.color_happy, "Comment 1", "Date 1"))
+        testlist.add(DailyMood("Mood 1", R.mipmap.smiley_happy, R.color.color_happy, "Comment 1", "Date 1"))
+        testlist.add(DailyMood("Mood 2", R.mipmap.smiley_sad, R.color.color_sad, "Comment 2", "Date 2"))
+    }
+    
 }
