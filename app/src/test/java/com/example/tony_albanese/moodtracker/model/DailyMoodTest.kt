d61e1ee84fc2e318d2ebfae7a9228a914206ece3
tony-albanese <tony.albanese2000@gmail.com>
2018-07-10 19:59:37 +0200

    Created UnitTest files

---
+++ app/src/test/java/com/example/tony_albanese/moodtracker/model/DailyMoodTest.kt
@@ -0,0 +1,23 @@
+package com.example.tony_albanese.moodtracker.model
+
+import org.junit.Test
+
+class DailyMoodTest {
+
+    @Test
+    fun getMComment() {
+
+    }
+
+    @Test
+    fun setMComment() {
+    }
+
+    @Test
+    fun getMDate() {
+    }
+
+    @Test
+    fun setMDate() {
+    }
+}
---
+++ app/src/test/java/com/example/tony_albanese/moodtracker/model/MoodTest.kt
@@ -0,0 +1,31 @@
+package com.example.tony_albanese.moodtracker.model
+
+import org.junit.Test
+
+class MoodTest {
+
+    @Test
+    fun getMDescription() {
+
+    }
+
+    @Test
+    fun setMDescription() {
+    }
+
+    @Test
+    fun getMImageId() {
+    }
+
+    @Test
+    fun setMImageId() {
+    }
+
+    @Test
+    fun getMBackgoundColor() {
+    }
+
+    @Test
+    fun setMBackgoundColor() {
+    }
+}
