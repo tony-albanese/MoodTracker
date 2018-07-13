310d381f844655fda778cf43b4620f26f8d2d986
tony-albanese <tony.albanese2000@gmail.com>
2018-07-13 14:20:45 +0200

    Created unit test for saveCommentToSharedPreferences.

--- app/src/androidTest/java/com/example/tony_albanese/moodtracker/controller/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/tony_albanese/moodtracker/controller/MainActivityTest.kt
@@ -1,7 +1,11 @@
 package com.example.tony_albanese.moodtracker.controller
 
+import android.content.Context
+import android.content.SharedPreferences
+import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
 import org.junit.After
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -9,10 +13,14 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
 
+    lateinit var preferences: SharedPreferences
+
+    val PREF_KEY = "KEY"
 
     @Before
     fun setUp() {
-
+        val context = InstrumentationRegistry.getContext()
+        preferences = context.getSharedPreferences("TEST_PREFERENCES", Context.MODE_PRIVATE)
     }
 
     @After
@@ -20,9 +28,11 @@ class MainActivityTest {
     }
 
     @Test
-    fun writeSharedPreferences() {
-        
-
+    fun saveCommentSharedPreferencesTest() {
+        val string1 = "Test 1"
+        saveCommentToSharedPrefeences(preferences, PREF_KEY, string1)
+        val string2 = preferences.getString(PREF_KEY, "")
+        assertEquals(string1, string2)
     }
 
 }
