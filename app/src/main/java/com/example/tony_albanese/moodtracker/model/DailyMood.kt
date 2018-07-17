523b64437eeb574373821bb87046235d53d85997
tony-albanese <tony.albanese2000@gmail.com>
2018-07-17 20:15:23 +0200

    Added compareTo function to DailyMood class.

--- app/src/main/java/com/example/tony_albanese/moodtracker/model/DailyMood.kt
+++ app/src/main/java/com/example/tony_albanese/moodtracker/model/DailyMood.kt
@@ -3,12 +3,16 @@ package com.example.tony_albanese.moodtracker.model
 import java.io.Serializable
 
 //Class to represent a DailyMood object. This is the object inherits from Mood.
-class DailyMood(description: String = "No description specified", imageID: Int, backgroundColor: Int, comment: String = "", date: String) : Mood(description, imageID, backgroundColor), Serializable {
+class DailyMood(description: String = "No description specified", imageID: Int, backgroundColor: Int, comment: String = "", date: String) : Mood(description, imageID, backgroundColor), Serializable, Comparable<DailyMood> {
 
     var mComment: String = comment //Opional comment to go along with the mood.
     var mDate: String //A string representing the date the mood was recorded.
 
     init {
         mDate = date
+    }
+
+    override fun compareTo(other: DailyMood): Int {
+     return 0
     }
 }
