8101b0a101ce7bc4bd6aa6ba76000791ad9499f5
tony-albanese <tony.albanese2000@gmail.com>
2018-07-17 09:05:30 +0200

    Created test class and copied outline.

---
+++ app/src/androidTest/java/com/example/tony_albanese/moodtracker/controller/MainActivityUITest.java
@@ -0,0 +1,19 @@
+package com.example.tony_albanese.moodtracker.controller;
+
+
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityUITest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    
+}
